d7670c23b8ef719d3f6a512e8b3f0a82bf81e8e8
Jesse <ibbraidejesse@gmail.com>
2022-11-14 16:23:43 +0100

    exported to csv file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+''' export data in the CSV format '''
+from sys import argv
+from requests import get
+import csv
+
+
+if __name__ == '__main__':
+    endpoint = 'https://jsonplaceholder.typicode.com'
+    user_res = get(endpoint + '/users/' + argv[1]).json()
+    todos = get(endpoint + '/todos?userId=' + argv[1]).json()
+    usr = user_res['username']
+
+    with open('{}.csv'.format(argv[1]), 'w') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for todo in todos:
+            writer.writerow([argv[1], usr, todo['title'], todo['completed']])
+
+
+
