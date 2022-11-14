a8e12d28f21bea7002adc2f5b2cccad0b3297630
Jesse <ibbraidejesse@gmail.com>
2022-11-14 15:36:29 +0100

    refactored code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,8 +9,7 @@ if __name__ == '__main__':
     user_res = requests.get(endpoint + '/users/' + argv[1]).json()
 
     # get total number of tasks
-    todos = requests.get(endpoint + '/todos?userId=' + argv[1]).json()
-    all_tasks = len(todos)
+    all_tasks = len(requests.get(endpoint + '/todos?userId=' + argv[1]).json())
 
     # get number completed tasks and their titles
     titles_done = [todo['title'] for todo in todos if todo['completed']]
