0e20cccc15c0709189c7a55f8d277921c608a24c
Jesse <ibbraidejesse@gmail.com>
2022-11-14 20:47:36 +0100

    dumped all records

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+''' export data in json format '''
+from sys import argv
+from requests import get
+import json
+
+
+if __name__ == '__main__':
+    endpoint = 'https://jsonplaceholder.typicode.com'
+    users = get(endpoint + '/users/').json()
+    todos = get(endpoint + '/todos/').json()
+
+    i, records = 0, []
+    id, user, all = users[i]['id'], users[i]['username'], {}
+
+    for todo in todos:
+        if todo['userId'] != id:
+            all[id] = records
+            i += 1
+            id, user = users[i]['id'], users[i]['username']
+            records = []
+        records.append({'username': user, 'task': todo['title'],
+                        'completed': todo['completed']})
+
+    all[id] = records
+    with open('todo_all_employees.json', 'w') as file:
+        json.dump(all, file)
+
+
