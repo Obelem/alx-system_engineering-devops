c0027d32c058952e1b9c468e775371df3731d692
Jesse <ibbraidejesse@gmail.com>
2022-11-14 16:31:19 +0100

    edited output formatting

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     usr = get(endpoint + '/users/' + argv[1]).json()['username']
     todos = get(endpoint + '/todos?userId=' + argv[1]).json()
 
-    with open('{}.csv'.format(argv[1]), 'w', newline='') as file:
+    with open('{}.csv'.format(argv[1]), 'w') as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
         for todo in todos:
             writer.writerow([argv[1], usr, todo['completed'], todo['title']])
